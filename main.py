59cc8fa96cabf685ef66ad2e7bb9f4280f8a2cdc
fjmacb <64097941+fjmacb@users.noreply.github.com>
2021-09-27 11:34:44 +0200

    New pycar with small main file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Import a library of functions called 'pygame'
 import pygame
-
 from main_functions import *
+
 # Initialize the game engine
 pygame.init()
 
-#call main rouine
+#call main routine
 main2()
